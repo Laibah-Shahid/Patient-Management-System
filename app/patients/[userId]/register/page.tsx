dd2d5065da6d45e6ebeb23ffe3cb827e8746d6ff
Laibah-Shahid <laibahshahid18@gmail.com>
2024-12-23 22:01:32 +0500

    initial commit

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -24,7 +24,7 @@ const Register = async ({ params: { userId } }: SearchParamProps) => {
 
           <RegisterForm user={user} />
 
-          <p className="copyright py-12">&copy; {new Date().getFullYear()} MedixPulse. All rights reserved.</p>
+          <p className="copyright py-12">&copy; {new Date().getFullYear()} CarePulse. All rights reserved.</p>
         </div>
       </section>
 
