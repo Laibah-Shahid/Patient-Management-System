da6607923a4f3794be75b3e5bbe081457cb4c2ed
Laibah-Shahid <laibahshahid18@gmail.com>
2025-01-07 15:50:58 +0500

    initial commit

--- components/forms/AppointmentForm.tsx
+++ components/forms/AppointmentForm.tsx
@@ -101,6 +101,7 @@ export const AppointmentForm = ({
             cancellationReason: values.cancellationReason,
           },
           type,
+          timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone, // Add this
         };
 
         const updatedAppointment = await updateAppointment(appointmentToUpdate);
